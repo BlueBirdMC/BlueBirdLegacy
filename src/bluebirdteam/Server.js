c399b2a2bbd62f7c2727b479fca39f7019229b1b
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-03-02 11:33:44 +0300

    Update Server.js

--- src/bluebirdteam/Server.js
+++ src/bluebirdteam/Server.js
@@ -20,7 +20,7 @@ class Server{
         this.getLogger().info("Starting Server...");
         this.getLogger().info("Loading BlueBird.json");
         this.getLogger().info("This Server Is Running BlueBird Version 1.0!");
-        this.getLogger().info("BlueBird Is distributed under MIT License");
+        this.getLogger().info("BlueBird Is distributed under GPLv3 License");
         this.getLogger().info("Opening server on " + new Config("BlueBird.json", Config.JSON).get("interface") + ":" + new Config("BlueBird.json", Config.JSON).get("port"));
         this.getLogger().info("Done in (" + (Date.now() - start_time) + "ms).");
 	    let reader = new ConsoleCommandReader(this);
